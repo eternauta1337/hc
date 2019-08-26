192a13fefd1fc171f3982f2b1a34b5b6e0f10112
Alejandro J. Santander <palebluedot@gmail.com>
2019-08-26 08:37:14 -0600

    Implemented staking

--- contracts/HCVoting.sol
+++ contracts/HCVoting.sol
@@ -27,12 +27,20 @@ contract HCVoting is IForwarder, AragonApp {
     string internal constant ERROR_CAN_NOT_FORWARD         = "HCVOTING_CAN_NOT_FORWARD";
     string internal constant ERROR_NOT_ENOUGH_SUPPORT      = "HCVOTING_NOT_ENOUGH_SUPPORT";
     string internal constant ERROR_ALREADY_EXECUTED        = "HCVOTING_ALREADY_EXECUTED";
+    string internal constant ERROR_TOKEN_TRANSFER_FAILED   = "HCVOTING_TOKEN_TRANSFER_FAILED";
+    string internal constant ERROR_INSUFFICIENT_STAKE      = "HCVOTING_INSUFFICIENT_STAKE";
+    string internal constant ERROR_PROPOSAL_IS_CLOSED      = "HCVOTING_PROPOSAL_IS_CLOSED";
+
     /*
      * Events
      */
 
     event ProposalCreated(uint256 proposalId, address creator, string metadata);
     event VoteCasted(uint256 proposalId, address voter, bool supports);
+    event ProposalUpstaked(uint256 proposalId, address staker, uint256 amount);
+    event ProposalDownstaked(uint256 proposalId, address staker, uint256 amount);
+    event UpstakeWithdrawn(uint256 proposalId, address staker, uint256 amount);
+    event DownstakeWithdrawn(uint256 proposalId, address staker, uint256 amount);
 
     /*
      * Constants
@@ -53,12 +61,17 @@ contract HCVoting is IForwarder, AragonApp {
         uint256 totalYeas;
         uint256 totalNays;
         mapping (address => Vote) votes;
+        uint256 totalUpstake;
+        uint256 totalDownstake;
+        mapping (address => uint256) upstakes;
+        mapping (address => uint256) downstakes;
     }
 
     mapping (uint256 => Proposal) proposals;
     uint256 public numProposals;
 
     MiniMeToken public voteToken;
+    MiniMeToken public stakeToken;
 
     uint256 public supportPPM;
 
@@ -75,12 +88,13 @@ contract HCVoting is IForwarder, AragonApp {
      * Init
      */
 
-    function initialize(MiniMeToken _voteToken, uint256 _supportPPM) public onlyInit {
+    function initialize(MiniMeToken _voteToken, MiniMeToken _stakeToken, uint256 _supportPPM) public onlyInit {
         require(_supportPPM > 0, ERROR_INVALID_SUPPORT);
 
         initialized();
 
         voteToken = _voteToken;
+        stakeToken = _stakeToken;
         supportPPM = _supportPPM;
     }
 
@@ -102,6 +116,8 @@ contract HCVoting is IForwarder, AragonApp {
 
     function vote(uint256 _proposalId, bool _supports) public {
         Proposal storage proposal_ = _getProposal(_proposalId);
+
+        require(!proposal_.executed, ERROR_PROPOSAL_IS_CLOSED);
 
         uint256 userVotingPower = voteToken.balanceOfAt(msg.sender, proposal_.creationBlock);
         require(userVotingPower > 0, ERROR_NO_VOTING_POWER);
@@ -134,6 +150,22 @@ contract HCVoting is IForwarder, AragonApp {
         proposal_.votes[msg.sender] = _supports ? Vote.Yea : Vote.Nay;
 
         emit VoteCasted(_proposalId, msg.sender, _supports);
+    }
+
+    function upstake(uint256 _proposalId, uint256 _amount) public {
+        _stake(_proposalId, _amount, true);
+    }
+
+    function downstake(uint256 _proposalId, uint256 _amount) public {
+        _stake(_proposalId, _amount, false);
+    }
+
+    function withdrawUpstake(uint256 _proposalId, uint256 _amount) public {
+        _withdrawStake(_proposalId, _amount, true);
+    }
+
+    function withdrawDownstake(uint256 _proposalId, uint256 _amount) public {
+        _withdrawStake(_proposalId, _amount, false);
     }
 
     function executeProposal(uint256 _proposalId) public {
@@ -180,6 +212,26 @@ contract HCVoting is IForwarder, AragonApp {
         return proposal_.creationBlock;
     }
 
+    function getProposalUpstake(uint256 _proposalId) public view returns (uint256) {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+        return proposal_.totalUpstake;
+    }
+
+    function getProposalDownstake(uint256 _proposalId) public view returns (uint256) {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+        return proposal_.totalDownstake;
+    }
+
+    function getUpstake(uint256 _proposalId, address _user) public view returns (uint256) {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+        return proposal_.upstakes[_user];
+    }
+
+    function getDownstake(uint256 _proposalId, address _user) public view returns (uint256) {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+        return proposal_.downstakes[_user];
+    }
+
     /*
      * Forwarding
      */
@@ -200,6 +252,54 @@ contract HCVoting is IForwarder, AragonApp {
     /*
      * Internal
      */
+
+    function _stake(uint256 _proposalId, uint256 _amount, bool _upstake) internal {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+
+        require(!proposal_.executed, ERROR_PROPOSAL_IS_CLOSED);
+
+        if (_upstake) {
+            proposal_.totalUpstake = proposal_.totalUpstake.add(_amount);
+            proposal_.upstakes[msg.sender] = proposal_.upstakes[msg.sender].add(_amount);
+
+            emit ProposalUpstaked(_proposalId, msg.sender, _amount);
+        } else {
+            proposal_.totalDownstake = proposal_.totalDownstake.add(_amount);
+            proposal_.downstakes[msg.sender] = proposal_.downstakes[msg.sender].add(_amount);
+
+            emit ProposalDownstaked(_proposalId, msg.sender, _amount);
+        }
+
+        require(
+            stakeToken.transferFrom(msg.sender, address(this), _amount),
+            ERROR_TOKEN_TRANSFER_FAILED
+        );
+    }
+
+    function _withdrawStake(uint256 _proposalId, uint256 _amount, bool _upstake) internal {
+        Proposal storage proposal_ = _getProposal(_proposalId);
+
+        if (_upstake) {
+            require(getUpstake(_proposalId, msg.sender) >= _amount, ERROR_INSUFFICIENT_STAKE);
+
+            proposal_.totalUpstake = proposal_.totalUpstake.sub(_amount);
+            proposal_.upstakes[msg.sender] = proposal_.upstakes[msg.sender].sub(_amount);
+
+            emit UpstakeWithdrawn(_proposalId, msg.sender, _amount);
+        } else {
+            require(getDownstake(_proposalId, msg.sender) >= _amount, ERROR_INSUFFICIENT_STAKE);
+
+            proposal_.totalDownstake = proposal_.totalDownstake.sub(_amount);
+            proposal_.downstakes[msg.sender] = proposal_.downstakes[msg.sender].sub(_amount);
+
+            emit DownstakeWithdrawn(_proposalId, msg.sender, _amount);
+        }
+
+        require(
+            stakeToken.transfer(msg.sender, _amount),
+            ERROR_TOKEN_TRANSFER_FAILED
+        );
+    }
 
     function _calculatePPM(uint256 _votes, uint256 _total) internal pure returns (uint256) {
         return _votes.mul(MILLION).div(_total);

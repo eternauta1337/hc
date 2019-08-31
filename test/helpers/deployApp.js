df29e8929be47544e266ea79211b5d2b85c3f37b
Alejandro J. Santander <palebluedot@gmail.com>
2019-08-31 02:41:20 -0600

    Minor metadata changes

--- test/helpers/deployApp.js
+++ test/helpers/deployApp.js
@@ -68,7 +68,7 @@ const deployApp = async (dao, acl, appManager) => {
 
   // Instantiate a proxy for the app, using the base contract as its logic implementation.
   const instanceReceipt = await dao.newAppInstance(
-    hash('counter.aragonpm.test'), // appId - Unique identifier for each app installed in the DAO; can be any bytes32 string in the tests.
+    hash('hcvoting.aragonpm.test'), // appId - Unique identifier for each app installed in the DAO; can be any bytes32 string in the tests.
     appBase.address, // appBase - Location of the app's base implementation.
     '0x', // initializePayload - Used to instantiate and initialize the proxy in the same call (if given a non-empty bytes string).
     false, // setDefault - Whether the app proxy is the default proxy.

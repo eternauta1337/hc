a609bef1cffa8a9862c8cc126b942966bd04c0bb
Victor Porton <porton.victor@gmail.com>
2022-06-20 05:30:41 +0300

    truffle.js modified for a bug fix

--- truffle.js
+++ truffle.js
@@ -1,1 +1,15 @@
 module.exports = require('@aragon/os/truffle-config')
+
+module.exports = {
+    ...module.exports,
+    networks: {
+        ...module.exports.networks,
+        development: {
+          network_id: 15,
+          host: 'localhost',
+          port: 8545,
+          gas: 6.9e6,
+          gasPrice: 15000000001,
+        },
+    },
+}
